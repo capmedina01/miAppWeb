dc1e21893a1d94ef2e6578a1ccff5e2d1f276493
perdomomedina.carlos@gmail.com <perdomomedina.carlos@gmail.com>
2025-02-06 14:07:35 -0500

    subir proyecto a git

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+ 
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <p className="saludar">
+          HOLA MUNDO
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default App;
